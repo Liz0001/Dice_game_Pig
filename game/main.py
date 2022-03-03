31643429065ccdbf4935e3cc8a1a43e54e62c02e
Liis U <liis.usin@gmail.com>
2022-03-03 15:07:56 +0100

    Rules made prettier

--- game/main.py
+++ game/main.py
@@ -1,16 +1,18 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-
 """
-Let's play a game called "PIG".
+    Let's play a game called "PIG".
 
-There are 2 players: You and the opponent.
-
-Players take turns to roll a dice as many times as they wish,
-adding all roll results to a running total,
-but if the player rolls a 1 the gained score for the turn will be lost.
-
-Who gets to 100 points first wins the game.
++-----------------------------------------------------------+
+|  There are 2 players: You and the opponent.               |
+|                                                           |
+|  Players take turns to roll a dice as many times as       |
+|  they wish, adding all roll results to a running total,   |
+|  but if the player rolls a 1 the gained score for the     |
+|  turn will be lost.                                       |
+|                                                           |
+|  Who gets to 100 points first wins the game.              |
++-----------------------------------------------------------+
 """
 
 import shell
