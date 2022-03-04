6078d96d59b48f6912343177d7415763bbe79fcd
Anazeb <anam.aurangzeb@gmail.com>
2022-03-04 13:00:36 +0100

    Intelligence class implemented

---
+++ game/intelligence.py
@@ -0,0 +1,28 @@
+import random
+
+""" Creating the intellegence for the computer a s to how it would be playing and making decisions """
+
+class Intelligence:
+
+    def __init__(self) -> None:
+        """
+            Initializing the variables. It would not be returning anything
+            hence the return type is specified as none
+        """
+        random.seed(42)
+        self.CURRENT_SCORE_HOLD = 20
+        self.sum_scores = 0
+
+    def get_number_of_turns(self) -> int:
+        """
+            Randomly generating a number which would
+            signify the times the computer would roll
+        """
+        return random.randint(1, 10)
+
+    """  """
+    def when_to_hold(self, current_dice_value: int) -> bool:
+        self.sum_scores += current_dice_value
+        if self.sum_scores >= self.CURRENT_SCORE_HOLD:
+            return False
+        return True
--- test/test_intelligence.py
+++ test/test_intelligence.py
@@ -5,6 +5,7 @@
 
 import unittest
 from game import intelligence
+import random 
 
 
 class TestIntelligenceClass(unittest.TestCase):
@@ -12,6 +13,34 @@ class TestIntelligenceClass(unittest.TestCase):
 
     def test_init_default_object(self):
         """Instantiate an object and check its properties."""
+        res = intelligence.Intelligence()
+        exp = intelligence.Intelligence
+        self.assertIsInstance(res, exp)
+        self.assertEqual(res.CURRENT_SCORE_HOLD, 20)
+        self.assertNotEqual(res.CURRENT_SCORE_HOLD, 30)
+    
+    def test_get_numer_off_turns(self):
+        """ Testing the number of turns."""
+        intell = intelligence.Intelligence()
+        random.seed(42) # when randomly generating but the pattern would be the same. Can be any integer value
+        self.assertEqual(random.randint(1, 10), 2)
+        self.assertEqual(random.randint(1, 10), 1)# written twice to ensure that the result is same
+        self.assertEqual(random.randint(1, 10), 5)
+        self.assertEqual(random.randint(1, 10), 4)
+        self.assertNotEqual(random.randint(1, 10), 2)
+        
+
+    def test_when_to_hold(self):
+        """ Testing that the turn would be up if the score is 20 or more"""
+        intell = intelligence.Intelligence()
+        self.assertEqual(intell.when_to_hold(25), False)
+        self.assertEqual(intell.when_to_hold(20), False)
+        self.assertEqual(intell.when_to_hold(19), True)
+        self.assertNotEqual(intell.when_to_hold(36), True)
+        self.assertNotEqual(intell.when_to_hold(20), True)
+        self.assertNotEqual(intell.when_to_hold(18), False)
+
+
 
 
 if __name__ == "__main__":
