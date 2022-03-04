127893bb472a851ee7c3dc9603b45de5465e4129
Anazeb <anam.aurangzeb@gmail.com>
2022-03-04 17:44:25 +0100

    Dice and dice hand class

---
+++ game/dice.py
@@ -0,0 +1,34 @@
+
+import random
+
+
+class Dice:
+    
+    def __init__(self) -> None:
+        """ Initializing the values """
+        self.roll_count = 0
+        self.turn = 0
+        
+    def roll(self) -> int:
+        """ Generating a value randomly. Increasing a roll count """
+        
+        self.turn = random.randint(1,6)
+        self.roll_count += 1
+        return self.turn
+        
+    def __str__(self) -> str:
+        """ Priniting out the score and the roll count"""
+        return f"You have rolled {self.turn}.\n Your roll count is {self.roll_count}." 
+        
+        
+        
+        
+            
+                
+                
+            
+            
+            
+        
+        
+    
