2d1f9aa7d55d274480b33aed703fab44d2f30324
pigdicegame <100696456+pigdicegame@users.noreply.github.com>
2022-03-07 01:05:54 +0330

    Add score.py

--- game/score.py
+++ game/score.py
@@ -1,1 +1,24 @@
 # keep the history here
+
+from player import Player
+
+class score:
+    
+    _score = 0
+
+    def __init__(self, player: Player):
+        """Player initiated with default name value."""
+        self._score = player.get_score()
+
+    def keep_score(self, player_score):
+        self._score = player_score
+
+
+    def compare_score(self, player: Player):
+        if(self._score > player.get_score()):
+            print(player.get_name() + ' scored higher')
+        else:
+            print(player.get_name() + ' scored lower')
+
+    def show_score(self):
+        print(self._score)
