2dc2657a5adf51293bd8dad083994c37ad613473
Anazeb <anam.aurangzeb@gmail.com>
2022-03-10 09:37:59 +0100

    Dice hand and cheat implemented

--- game/dicehand.py
+++ game/dicehand.py
@@ -1,15 +1,31 @@
 from dice import Dice
-
+import random
 
 class DiceHand:
-    
+
     def __init__(self) -> None:
         self.dice = Dice()
-    
-    def keep_rolling(self):
-        """ Confirm the player if he wants to roll """
-        pass
+        self.check_turn = 1
 
-    def difficulty(self):
-        """Difficulty of the game."""
-        pass
+    def keep_rolling(self, input: bool):
+        """ Confirm the player if he wants to roll. """    
+        if input:
+            return self.dice.roll()
+        else:
+            self.dice.roll_count = 0
+            self.dice.turn = 0
+        return None
+    
+    def check_hand(self, cheat: bool):
+        if cheat:
+            if self.check_turn > 0:
+                self.check_turn -= 1
+                return random.randint(1, 6)
+            else:
+                raise Exception("Cheat available for this turn")
+        else:
+            return None
+                
+                
+        
+        
